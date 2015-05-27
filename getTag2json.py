3968e5db8baa4bd9873d6f4cdf7c8a1076e8301e
Caroline Chen <carolinetychen@gmail.com>
2015-05-27 17:52:03 +0800

    get image from instagram

--- getTag2json.py
+++ getTag2json.py
@@ -1,12 +1,35 @@
-import head
 import json
 import requests
+import sys
 
-url = "http://api.imagga.com/v1/tagging"
+import config
+import head
 
-querystring = {"url": "http://playground.imagga.com/static/img/example_photo.jpg"}
+tag = sys.argv[1]
+n = int(sys.argv[2])
+
+token = config.token
 headers = head.headers
-response = requests.request("GET", url, headers=headers, params=querystring)
-j = json.loads(response.text)
+instagramURL = "https://api.instagram.com/v1/tags/"+tag+"/media/recent?access_token="+token
+imaggaURL = "http://api.imagga.com/v1/tagging"
 
-json.dump(j, open("test.json", "w"), indent=2)
+imagga = []
+count = 0
+
+while n > 0:
+    if n < 20:
+        instagramURL += "&count=" + str(n)
+
+    req = requests.get(instagramURL)
+    instagramURL = json.loads(req.text)["pagination"]["next_url"]
+
+    for data in json.loads(req.text)["data"]:
+        imageURL = data["images"]["standard_resolution"]["url"]
+        querystring = {"url": str(imageURL)}
+        response = requests.request("GET", imaggaURL, headers=headers, params=querystring)
+        imagga += json.loads(response.text)["results"]
+
+    n -= 20
+
+fname = tag + ".json"
+json.dump(imagga, open(fname, "w"), indent=2)
